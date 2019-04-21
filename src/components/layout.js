57b0b86cb70e8b0637e32fe87170133741424284
Chris Renfrow <dev@chrisrenfrow.me>
2019-04-21 15:57:15 -0700

    Added gh-pages to dev dependencies. Minor tweaks

--- src/components/layout.js
+++ src/components/layout.js
@@ -64,7 +64,11 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built with {`<3`} by
+          {` `}
+          <a href="https://chrisrenfrow.me">Chris Renfrow</a>
+          {` `}
+          using
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
